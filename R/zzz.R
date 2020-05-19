cdf4f7a17eef929fdb6d4a0c3a4471e029b93aa5
jlopezper <jorge.loper@gmail.com>
2020-05-19 17:19:36 +0200

    add citation when load package

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage("\nPlease cite as: \n")
+  packageStartupMessage("Lopez, J. (2020), White's test and Bootstrapped White's test under the methodology of Jeong, J., Lee, K. (1999) package version 0.0.1")
+}
